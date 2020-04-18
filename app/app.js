6ea5da7de3cdd1c6d784610a84d2d105967858e3
Matthew Eller <mattcjeller@gmail.com>
2020-04-18 16:13:56 -0700

    Adds tsconfig.base

---
+++ app/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
