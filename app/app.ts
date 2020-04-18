14735840d64a9d66250934903ed80ce9fae7fa46
Matthew Eller <mattcjeller@gmail.com>
2020-04-18 15:08:11 -0700

    Stubs basic function

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,6 @@
 let nothing: string = "nothing for now";
+
+function startGame() {
+  var messagesElement = document.getElementById('messages');
+  messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
